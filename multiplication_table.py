0fd02a8a27c3aa70bebb9776a7f8747e392c775c
pranavrbm <pranavbm05@gmail.com>
2025-07-05 11:01:40 +0530

    program to display multiplication table {used different method than the usual }

--- multiplication_table.py
+++ multiplication_table.py
@@ -1,5 +1,11 @@
-def multi(n):
-    for i in range(1, 11):
-        print("%d * %d = %d" % (n,i, n*i))
+def multi(n,i=1):
+    # for i in range(1, 11):
+    #     print("%d * %d = %d" % (n,i, n*i))
+    if (i == 11):
+        return 
+    print(n, "*", i, "=", n * i)
+    i+=1
+    multi(n,i)
 if __name__ == "__main__":
-    multi(3)
+    n=3
+    multi(n)
