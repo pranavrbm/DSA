8fabaeaf6ba9b6f1454404df8cbc85ff05fff914
pranavrbm <pranavbm05@gmail.com>
2025-07-07 08:11:26 +0530

    Program to find the number closest to number n

--- odd_or_even.py
+++ odd_or_even.py
@@ -1,3 +1,17 @@
+'''
+Check whether a given number is even or odd
+
+Given a number n, check whether it is even or odd. Return true for even and false for odd.
+
+Examples: 
+
+Input: n = 15
+Output: false
+Explanation: 15 % 2 = 1, so 15 is odd .
+
+Input: n = 44
+Output: true
+Explanation: 44 % 2 = 0, so 44 is even.'''
 def isEven(n):
     if (n & 1) == 0:
         return True
--- sum_of_squares.py
+++ sum_of_squares.py
@@ -1,3 +1,17 @@
+'''
+Program for Sum of squares of first n natural numbers
+
+Given a positive integer n, we have to find the sum of squares of first n natural numbers. 
+Examples : 
+
+Input : n = 2
+Output: 5
+Explanation: 1^2+2^2 = 5
+
+Input : n = 8
+Output: 204
+Explanation :  1^2 + 2^2 + 3^2 + 4^2 + 5^2 + 6^2 + 7^2 + 8^2 = 204 
+'''
 def sums(n):
     # return sum([i**2 for i in range(1, n+1)])
     # return (n * (n+1) * (2*n+1)) //6
