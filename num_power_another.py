8fc68a73f1e09809106bb84c5d2fe35e3b8c7467
pranavrbm <pranavbm05@gmail.com>
2025-07-07 22:52:18 +0530

    Updated the program num_power_another.py added another method

--- num_power_another.py
+++ num_power_another.py
@@ -14,9 +14,12 @@ x^3 = 1
 Input:  x = 10, y = 1001
 Output: False
 '''
+import math
 
 
 def isPower(x, y):
+    # Method 1
+    '''
     if x == 1:
         return y == 1
     pow = 1
@@ -24,6 +27,14 @@ def isPower(x, y):
         pow *= x
 
     return y == pow
+    ''' 
+    # Method 2 
+    if x == 1:
+        return y == 1 
+    if y == 1:
+        return True
+    k = float(math.log(x) / math.log(y))
+    return (round(k) - k) <= 1e-10
 
 
 if __name__ == "__main__":
